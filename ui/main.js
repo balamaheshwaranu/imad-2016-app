7274e4937c392cbbff635b54b3786726d194ad5f
balamaheshwaranu <bala.bmw@gmail.com>
2016-10-17 00:14:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,33 @@
 console.log('Loaded!');
+var submitButton = document.getElementById("SubmitButton");
+var firstName = document.getElementById("firstName");
+var comments = document.getElementById("Comments");
+document.getElementById("comments").onclick = function() {myFunction()};
+
+function myFunction() {
+ var request = new XMLHttpRequest();
+ 
+ request.onreadystatechange = function(){
+     if(request.readystate === XMLHttpRequest.DONE){
+         if (request.status === 200){
+             var names = request.responseText;
+             names.JSON.parse(names);
+             var list = '';
+             list = '<ul>';
+             for (var i=0;i<names.length;i++){
+                 list += '<li>'+names[i]+'</li>';
+            }
+            list += '</ul>';
+            var reslt = document.getElementById("Results");
+            reslt.innerHTML = list;
+         }
+     }
+     
+ };
+ var name = firstName+':'+comments;
+ request.open('GET','http://http://balamaheshwaranu.imad.hasura-app.io/commentsIncrement?name='+name,true);
+ request.send(null);
+}
+
+
+
